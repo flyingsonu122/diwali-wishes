f8b8ad28a90c09f567cabdbfd5abcdc06d6bb4f7
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-11-03 11:14:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-
 const Wish = document.getElementById('demo');
 const btn = document.getElementById('btn');
+const twt = document.getElementById('twt');
+const fb = document.getElementById('fb');
+const mail = document.getElementById('mail');
+const whatsapp = document.getElementById('whatsapp');
 
-btn.addEventListener('click',getWish);
+btn.addEventListener('click', getWish);
 
 getWish();
 
-function getWish(){
+function getWish() {
     fetch("https://www.sonu.live/JSON/db.json")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // console.log(data);
-    let index = Math.floor(Math.random() * data.length);
-    // console.log(index);
-    Wish.textContent = data[index].wish;
-    
-  });
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // console.log(data);
+            let index = Math.floor(Math.random() * data.length);
+            // console.log(index);
+            var wish = data[index].wish;
+            Wish.textContent = wish;
+
+
+
+            // links
+
+            twt.href = `https://twitter.com/intent/tweet?text=${wish}`;
+            fb.href = `https://facebook.com/sharer/sharer.php?u=${wish}`;
+            mail.href = `mailto:?subject=Happy%20Diwali.&amp;body=${wish}`;
+            whatsapp.href = `https://api.whatsapp.com/send/?text=${wish}`;
+
+
+        });
 }
